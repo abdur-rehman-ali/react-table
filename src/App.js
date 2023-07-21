d157b9deae07c0e4b903278175a85d2946955539
abdur-rehman-ali <alijoyia911@gmail.com>
2023-07-22 01:05:27 +0500

    Create a basic table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import BasicTable from './components/BasicTable';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <BasicTable />
     </div>
   );
 }
